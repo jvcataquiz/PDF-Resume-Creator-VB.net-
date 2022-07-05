644e5ad930820d2369a4fbc22c3d1ed23f3e620f
jvcataquiz <jvcataquiz@gmail.com>
2022-07-05 18:28:09 +0800

    i put all the data i needed in the pdf to create a resume, for now imgonna arrange and dedign the align of every data

--- PDF Resume Creator/PDF Resume Creator/PDF Resume Creator.vb
+++ PDF Resume Creator/PDF Resume Creator/PDF Resume Creator.vb
@@ -3,6 +3,7 @@ Imports System.Text
 Imports Newtonsoft.Json
 Imports iTextSharp.text
 Imports iTextSharp.text.pdf
+Imports iTextSharp.text.pdf.draw
 Public Class PDFResumeCreator
     Dim path As String
     Dim filedata As String
@@ -15,7 +16,7 @@ Public Class PDFResumeCreator
 
     Private Sub ButtonSaveasJSON_Click(sender As Object, e As EventArgs) Handles ButtonSaveasJSON.Click
         path = "JSONFILE/" & TextBoxFullname.Text & ".json"
-
+        Dim bday As String = DateTimePickerBirthday.Value.Month & DateTimePickerBirthday.Value.Day & DateTimePickerBirthday.Value.Year
         Dim customers As List(Of information) = New List(Of information)()
 
         Dim customer As information = New information With {
@@ -23,7 +24,7 @@ Public Class PDFResumeCreator
             .Email = TextBoxEmail.Text,
             .PhoneNumber = TextBoxPhoneNumber.Text,
             .Gender = ComboBoxGender.Text,
-            .Birthday = DateTimePickerBirthday.Value.ToString(),
+            .Birthday = bday,
             .Address = RichTextBoxAddress.Text,
             .Objective = RichTextBoxObjective.Text,
             .College = TextBoxGrad.Text,
@@ -61,6 +62,58 @@ Public Class PDFResumeCreator
 
         jsonWriter.Open()
         jsonWriter.Add(New Paragraph(record.Name))
+        Dim name As Paragraph = New Paragraph(record.Name.ToUpper())
+        Dim email As Paragraph = New Paragraph(record.Email)
+        Dim contact As Paragraph = New Paragraph(record.PhoneNumber)
+        Dim address As Paragraph = New Paragraph(record.Address)
+        Dim birthday As Paragraph = New Paragraph(record.Birthday & vbLf & vbLf)
+        Dim obj As Chunk = New Chunk(vbLf & vbLf & "OBJECTIVE:" & vbLf)
+        Dim objective As Paragraph = New Paragraph(record.Objective & vbLf & vbLf)
+        Dim edu As Chunk = New Chunk(vbLf & "EDUCATIONAL BACKGROUND:" & vbLf & vbLf)
+        Dim college As Paragraph = New Paragraph("COLLEGE: " & record.College & vbLf & vbLf)
+        Dim universi As Paragraph = New Paragraph("COLLEGE: " & record.University & vbLf & vbLf)
+        Dim sy As Paragraph = New Paragraph("COLLEGE: " & record.Schoolyear & vbLf & vbLf)
+        Dim skill As Chunk = New Chunk(vbLf & "SKILLS:" & vbLf & vbLf)
+        Dim _skill1 As Paragraph = New Paragraph("* " & record.Skill1 & vbLf & vbLf)
+        Dim _skill2 As Paragraph = New Paragraph("* " & record.Skill2 & vbLf & vbLf)
+        Dim _skill3 As Paragraph = New Paragraph("* " & record.Skill3 & vbLf & vbLf)
+        Dim _skill4 As Paragraph = New Paragraph("* " & record.Skill4 & vbLf & vbLf)
+
+        Dim line As LineSeparator = New LineSeparator(5.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_RIGHT, 1)
+
+        name.Font.Size = 20
+        name.Alignment = Element.ALIGN_CENTER
+        name.SetLeading(15, 1)
+        obj.Font.Size = 18
+        edu.Font.Size = 18
+
+        objective.SetLeading(15, 1)
+
+        college.IndentationLeft = 40
+
+        skill.Font.Size = 18
+        _skill1.IndentationLeft = 40
+        _skill2.IndentationLeft = 40
+        _skill3.IndentationLeft = 40
+        _skill4.IndentationLeft = 40
+
+
+        jsonWriter.Add(name)
+        jsonWriter.Add(contact)
+        jsonWriter.Add(address)
+        jsonWriter.Add(email)
+        jsonWriter.Add(birthday)
+        jsonWriter.Add(obj)
+        jsonWriter.Add(objective)
+        jsonWriter.Add(edu)
+
+        jsonWriter.Add(college)
+        jsonWriter.Add(skill)
+        jsonWriter.Add(_skill1)
+        jsonWriter.Add(_skill2)
+        jsonWriter.Add(_skill3)
+        jsonWriter.Add(_skill4)
+        jsonWriter.Add(line)
         jsonWriter.Close()
 
     End Sub
